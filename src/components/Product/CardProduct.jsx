756a9f33f93300a6dd6883b54ab053d4e8192e66
ErikSilva231 <erik.silva.fuentes@gmail.com>
2024-02-09 18:05:51 -0300

    .

--- src/components/Product/CardProduct.jsx
+++ src/components/Product/CardProduct.jsx
@@ -1,48 +1,14 @@
-/* eslint-disable react/prop-types */
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../../assets/css/CardProduct.css";
-import { useNavigate } from "react-router-dom";
 
 const CardProduct = ({ id, img, nombre, descripcion, precio }) => {
-  const navigate = useNavigate();
-
-  const irAdetalle = (id) => {
-    navigate(`/productDetail/${id}`);
-  };
-
   return (
-    <div className="container page-wrapper">
-      <div className="page-inner">
-        <div className="row">
-          <div className="el-wrapper">
-            <div className="box-up">
-              <img
-                className="img"
-                src={img}
-                alt=""
-                onClick={() => irAdetalle(id)}
-              />
-              <div className="img-info">
-                <div className="info-inner">
-                  <span className="p-name">{nombre}</span>
-                </div>
-                <div className="a-size">
-                  <span className="size">{descripcion}</span>
-                </div>
-              </div>
-            </div>
-            <div className="box-down">
-              <div className="h-bg">
-                <div className="h-bg-inner"></div>
-              </div>
-              <a className="cart" href="#">
-                <span className="price">{precio}</span>
-                <span className="add-to-cart">
-                  <span className="txt">Agregar al carro</span>
-                </span>
-              </a>
-            </div>
-          </div>
+    <div className="col-sm-12 col-md-6 col-xl-3 mt-5">
+      <div className="card">
+        <img src={img} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{nombre}</h5>
+          <p className="card-text">{descripcion}</p>
+          <p className="card-text text-center fw-bold bg-light py-2">${precio}</p>
         </div>
       </div>
     </div>
--- src/components/Product/SectionProduct.jsx
+++ src/components/Product/SectionProduct.jsx
@@ -1,27 +1,34 @@
 import { useContext } from "react";
 import { Context } from "../../context/Context";
+import { useNavigate } from "react-router-dom";
 import CardProduct from "./CardProduct";
-import "../../assets/css/SectionProduct.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export const SectionProduct = () => {
   const { productos, carrito, setCarrito } = useContext(Context);
 
+  const navigate = useNavigate();
+
+  const irAproductos = (id) => {
+    navigate(`/productoDetail/${id}`);
+  };
+
   return (
     <>
-      <section>
-        <div className="contenedor-cards">
+       <div className="container">
+      <div className="row">
           {productos.map((producto) => (
             <CardProduct
               key={producto.id}
-              id={producto.id}
               nombre={producto.nombre}
               descripcion={producto.descripcion}
-              img={producto.img}
+              img={producto.url_imagen}
               precio={producto.precio}
             />
           ))}
         </div>
-      </section>
+        </div>
     </>
   );
 };
