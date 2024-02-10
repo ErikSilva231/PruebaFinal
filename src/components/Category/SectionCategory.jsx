664074e0f8eea87b756d8854179cf68c971bca8b
ErikSilva231 <erik.silva.fuentes@gmail.com>
2024-02-09 21:38:53 -0300

    Update SectionCategory.jsx

--- src/components/Category/SectionCategory.jsx
+++ src/components/Category/SectionCategory.jsx
@@ -1,4 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from 'react-router-dom';
+
 
 function SectionCategory() {
   return (
@@ -11,47 +13,65 @@ function SectionCategory() {
               className="card-img-top"
               alt="..."
             />
-                                 <h5 type="button" href="#" className="position-absolute top-100 start-50 text-center translate-middle bg-secondary z-3 py-2 px-2">BARRAS DE PROTEINAS</h5>
-
+            <h5
+              type="button"
+              href="#"
+              className="position-absolute top-100 start-50 text-center translate-middle bg-secondary z-3 py-2 px-2"
+            > <Link className="nav-link" to="/productoDetail/${id}">BARRAS DE PROTEINAS</Link>
+              
+            </h5>
           </div>
+        </div>
 
-          
-          </div>
-          
-      <div className="col-sm-12 col-md-6 col-xl-3 mt-5">
+        <div className="col-sm-12 col-md-6 col-xl-3 mt-5">
           <div className="card">
             <img
               src="../../../public/Cacao_500x200.png"
               className="card-img-top"
               alt="..."
             />
-                                 <h5 type="button" href="#" className="position-absolute top-100 start-50 text-center translate-middle bg-secondary z-3 py-2 px-4">CACAO</h5>
-
+            <h5
+              type="button"
+              href="#"
+              className="position-absolute top-100 start-50 text-center translate-middle bg-secondary z-3 py-2 px-4"
+            >
+             <Link className="nav-link" to="/productoDetail/${id}">CACAO</Link>
+            </h5>
           </div>
-          </div>
-          <div className="col-sm-12 col-md-6 col-xl-3 mt-5">
+        </div>
+        <div className="col-sm-12 col-md-6 col-xl-3 mt-5">
           <div className="card">
             <img
               src="../../../public/FRUTOS_SECOS_500x200.png"
               className="card-img-top"
               alt="..."
             />
-                                 <h5 type="button" href="#" className="position-absolute top-100 start-50 text-center translate-middle bg-secondary z-3 py-2 px-4">FRUTOS SECOS</h5>
-
+            <h5
+              type="button"
+              href="#"
+              className="position-absolute top-100 start-50 text-center translate-middle bg-secondary z-3 py-2 px-4"
+            >
+              <Link className="nav-link" to="/productoDetail/${id}">FRUTOS SECOS</Link>
+            </h5>
           </div>
-          </div>
-          <div className="col-sm-12 col-md-6 col-xl-3 mt-5">
+        </div>
+        <div className="col-sm-12 col-md-6 col-xl-3 mt-5">
           <div className="card">
             <img
               src="../../../public/te_500x200.png"
               className="card-img-top"
               alt="..."
             />
-                                 <h5 type="button" href="#" className="position-absolute top-100 start-50 text-center translate-middle bg-secondary z-3 py-2 px-4">INFUSIONES</h5>
-
+            <h5
+              type="button"
+              href="#"
+              className="position-absolute top-100 start-50 text-center translate-middle bg-secondary z-3 py-2 px-4"
+            >
+              <Link className="nav-link" to="/productoDetail/${id}">INFUSIONES</Link>
+            </h5>
           </div>
-          </div>
-    </div>
+        </div>
+      </div>
     </div>
   );
 }
