8a1a44842604a2d07a34ea137ba8a2d69b3529dc
Bastian Gutierrez <bas.gutierrez@gmail.com>
2024-02-11 17:25:46 -0300

    se agrega validación de formatos  email y password

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,23 +1,31 @@
 import { useState } from "react";
 
 function Login() {
-  const [user, setUser] = useState({ email: "", passwor: "" });
+  const [user, setUser] = useState({ email: "", password: "" });
+  const [validPass, setValidPass] = useState(true);
+  const [validEmail, setValidEmail] = useState(true);
   const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  const classInputPass = `form-control ${validPass ? "" : "is-invalid"}`;
+  const classInputEmail = `form-control ${validEmail ? "" : "is-invalid"}`;
 
-  const handleUser = (event) =>
+  const handleUser = (event) => {
     setUser({ ...user, [event.target.name]: event.target.value });
-
+  };
   const handleForm = (event) => {
     event.preventDefault();
+    setValidEmail(true);
+    setValidPass(true);
 
     if (!user.email.trim() || !user.password.trim()) {
+      setValidEmail(false);
+      setValidPass(false);
       return window.alert("Email y password obligatorias.");
     }
 
     if (!emailRegex.test(user.email)) {
+      setValidEmail(false);
       return window.alert("El formato del email no es correcto!");
     }
-    //Logica para llamar a la api para registrarse
   };
   return (
     <main className="container  d-flex justify-content-center align-items-center p-5">
@@ -30,8 +38,9 @@ function Login() {
           <label className="form-lebel">Correo Electrónico</label>
           <input
             type="email"
+            name="email"
             onChange={handleUser}
-            className="form-control"
+            className={classInputEmail}
             placeholder="Ingresa tu correo electrónico"
           />
         </div>
@@ -39,8 +48,9 @@ function Login() {
           <label className="form-lebel">Contraseña</label>
           <input
             type="password"
+            name="password"
             onChange={handleUser}
-            className="form-control"
+            className={classInputPass}
             placeholder="Ingresa tu contraseña"
           />
           <a href="">¿Olvidaste tu contraseña?</a>
