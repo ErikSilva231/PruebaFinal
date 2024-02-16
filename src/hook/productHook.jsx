11c3690e3211a1b273fb2014e088e51d748764f5
Bastian Gutierrez <bas.gutierrez@gmail.com>
2024-02-16 00:34:33 -0300

    Creación de hook de producto

---
+++ src/hook/productHook.jsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export const useProduct = (id) => {
+  const [producto, setProducto] = useState({
+    id: "",
+    name: "",
+    img_url: "",
+    description: "",
+    price: 0,
+    stock: 0,
+    category: "",
+  });
+  useEffect(() => {
+    getProducto(id).then((product) => setProducto(product[0]));
+  }, [id]);
+  return producto;
+};
+
+const getProducto = async (id) => {
+  const URLBASE = "http://localhost:3000";
+  try {
+    const response = await axios.get(URLBASE + `/products/${id}`);
+    return await response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
