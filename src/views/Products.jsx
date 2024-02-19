1b24a5c14ea37927bd8ffcab0f7a830cc5c95d06
fvaldivieso21 <fvaldivieso21@gmail.com>
2024-02-18 21:26:22 -0300

    Cambio en Componente SectionProduct

--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -2,17 +2,17 @@
 import { useParams } from "react-router-dom";
 import SectionCategory from "../components/Category/SectionCategory";
 import { SectionProduct } from "../components/Product/SectionProduct";
-import { category } from "../config/const";
+// import { category } from "../config/const";
 const Products = () => {
   const { categoria } = useParams();
-  const { name } = category.find(
+  /* const { name } = category.find(
     (cat) => cat.path === `/products/${categoria}`
-  );
+  ); */
 
   return (
     <>
       <SectionCategory />
-      <SectionProduct categoria={name} />
+      <SectionProduct categoria={categoria} />
     </>
   );
 };
