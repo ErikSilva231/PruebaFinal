4de6ca11d29ec7fedb9b6fc91a807a23b164ba2b
ErikSilva231 <erik.silva.fuentes@gmail.com>
2024-02-22 19:27:39 -0300

    img link

--- src/config/const.js
+++ src/config/const.js
@@ -8,28 +8,28 @@ export const ENDPOINT = {
 export const category = [{
     id: 1,
     name: "BARRA DE PROTEINAS",
-    src: "BARRA_500x200.png",
+    src: "https://i.ibb.co/bvH695T/BARRA-500x200.png",
     endpoint: "",
     path: "/products/BarradeProteinas"
 },
 {
     id: 2,
     name: "CACAO",
-    src: "Cacao_500x200.png",
-    endpoint: "",
+    src: "https://i.ibb.co/0nSjRys/Cacao-500x200.png",
+    endpoint: "FRUTOS_SECOS_500x200.png",
     path: "/products/Cacao"
 },
 {
     id: 3,
     name: "FRUTOS SECOS",
-    src: "FRUTOS_SECOS_500x200.png",
+    src: "https://i.ibb.co/ZY4NK3r/FRUTOS-SECOS-500x200.png",
     endpoint: "",
     path: "/products/FrutosSecos"
 },
 {
     id: 4,
     name: "INFUSIONES",
-    src: "te_500x200.png",
+    src: "https://i.ibb.co/TttLj2W/te-500x200.png",
     endpoint: "",
     path: "/products/Infusiones"
 }]
