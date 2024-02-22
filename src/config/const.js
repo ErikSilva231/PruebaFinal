1060b73d566cfad7d0ec75139f31090e0b06f651
ErikSilva231 <erik.silva.fuentes@gmail.com>
2024-02-22 19:19:24 -0300

    nombre guion

--- src/config/const.js
+++ src/config/const.js
@@ -8,28 +8,28 @@ export const ENDPOINT = {
 export const category = [{
     id: 1,
     name: "BARRA DE PROTEINAS",
-    src: "BARRA-500x200.png",
+    src: "BARRA_500x200.png",
     endpoint: "",
     path: "/products/BarradeProteinas"
 },
 {
     id: 2,
     name: "CACAO",
-    src: "Cacao-500x200.png",
+    src: "Cacao_500x200.png",
     endpoint: "FRUTOS_SECOS_500x200.png",
     path: "/products/Cacao"
 },
 {
     id: 3,
     name: "FRUTOS SECOS",
-    src: "FRUTOS-SECOS-500x200.png",
+    src: "FRUTOS_SECOS_500x200.png",
     endpoint: "",
     path: "/products/FrutosSecos"
 },
 {
     id: 4,
     name: "INFUSIONES",
-    src: "te-500x200.png",
+    src: "te_500x200.png",
     endpoint: "",
     path: "/products/Infusiones"
 }]
